9816590437d62b204fa4b16a6127a5fd340c0cb5
Aman-Rana-ls <suppa0173@gmail.com>
2025-01-08 11:05:57 +0530

    [Aman Rana] : Added the basic Program.

---
+++ program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace AddressBook_Final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book");
+            Console.ReadKey();  
+        }
+    }
+}
